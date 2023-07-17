6c389fe6fd784756bd3621d227f1d5e19b7c03fb
Likhitha <nlikhitha63@gmail.com>
2023-07-17 16:29:58 +0530

    Day 24:Add footers

--- pdf.py
+++ pdf.py
@@ -12,7 +12,8 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     #  set the footer
     pdf.ln(265)
@@ -27,5 +28,7 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="B", size=8)
         pdf.set_text_color(100, 100, 100)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
